7fc76dc5f22bc09b520ef987e1b43e1d840e14e5
ABHINASH <abhiofficial453@gmail.com>
2025-11-10 21:43:53 +0530

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: "https://mern-project-backend-file.onrender.com/", // 👈 must match your backend route prefix
+  baseURL: "https://mern-project-backend-file.onrender.com", // 👈 must match your backend route prefix
   //baseURL: "http://localhost:8000", // 👈 must match your backend route prefix
   // withCredentials: true,
 });
+
